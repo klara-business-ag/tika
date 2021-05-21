5e8e2991747b2df76bf343cc939ac651047fd739
Simon-Shlomo Poil <simon.poil@klara.ch>
2021-05-21 12:30:40 +0200

    moving the imports back

--- tika-parsers/tika-parsers-classic/tika-parsers-classic-modules/tika-parser-zip-commons/src/main/java/org/apache/tika/detect/zip/StreamingZipContainerDetector.java
+++ tika-parsers/tika-parsers-classic/tika-parsers-classic-modules/tika-parser-zip-commons/src/main/java/org/apache/tika/detect/zip/StreamingZipContainerDetector.java
@@ -23,6 +23,10 @@ import org.apache.tika.io.LookaheadInputStream;
 import org.apache.tika.metadata.Metadata;
 import org.apache.tika.mime.MediaType;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
 /**
  * Currently only used in tests.  Unlike {@link DefaultZipContainerDetector},
  * this will never try to open the File as a ZipFile; this relies solely
